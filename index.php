eccb507f5a8636c7bc58bff1bf56942f15809b7c
João Brito <jmvb070804@gmail.com>
2023-08-31 21:37:56 +0100

    add correct css and fixed security bugs
    added css code to make the page like the mockup presented. Fixed a security bug about the PHP htmlspecialchars.

--- index.php
+++ index.php
@@ -1,48 +1,62 @@
 <?php
-	require_once 'DB_connection.php';
-  	$sql = "SELECT * FROM carros";
-	$all_product = $conn->query($sql);
+    require_once 'DB_connection.php';
+    $sql = "SELECT * FROM carros";
+    $result = $conn->query($sql);
+
+    // Check for errors
+    if (!$result) {
+        die("Database query failed: " . $conn->error);
+    }
 ?>
 
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>OTTO'S DEALERSHIP</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>OTTO'S DEALERSHIP</title>
 
-<!--external files -->
-<link rel="stylesheet" href="css_geral.css">
+    <!-- External files -->
+    <link rel="stylesheet" href="css_geral.css">
 
-<!-- Fonts -->
-<link rel="stylesheet" href="https://use.typekit.net/nap8sij.css">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://use.typekit.net/nap8sij.css">
 </head>
 
 <body>
-	<?php
-		include_once 'header.php';	
-	?>
-	
-	<main>
-       <?php
-          while($row = mysqli_fetch_assoc($all_product)){
-       ?>
-		<div class="card">	
-			<div class="image">
-				<img src="<?php echo $row["Image"]; ?>" alt="">
-			</div>
-			
-		<div class="caption">
-			<p class="product_name"> <?php echo $row["name"]; ?></p>
-			<p class="price"><b>$<?php echo $row["Price"]; ?>?</b></p>
-		</div>
-		
-		</div>	
-		<?php
-		}
-		?>
-		
-	</main>
+    <?php include_once 'header.php'; ?>
+
+    <main>
+        <?php
+        while ($row = $result->fetch_assoc()) {
+        ?>
+            <div class="card">
+                <div class="image">
+                    <img src="<?php echo htmlspecialchars($row["Image"]); ?>" alt="Car Image">
+                </div>
+
+                <div class="caption">
+                    <p class="product_name"><b><?php echo htmlspecialchars($row["name"]); ?></b></p>
+                </div>
+
+                <div class="lower-caption">
+					<div class="buttons">
+                    	<a href="#" class="tag-button"><?php echo htmlspecialchars($row["Tag"]); ?></a>
+                    	<a href="#" class="tag-button">Classe <?php echo htmlspecialchars($row["Classe"]); ?></a>
+                    	<p class="price"><b>$<?php echo htmlspecialchars($row["Price"]); ?></b></p>
+					</div>
+				</div>
+            </div>
+        <?php
+        }
+        $result->free(); // Free up the result set
+        ?>
+
+    </main>
+    
+    <?php
+    $conn->close(); // Close the database connection
+    ?>
 
 </body>
 </html>
